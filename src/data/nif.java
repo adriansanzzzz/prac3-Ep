08bda43f1eb1f6d9056abf374628d7014eee9cd0
adriansanzzzz <74414073+adriansanzzzz@users.noreply.github.com>
2022-12-26 14:38:06 +0100

    Nif

--- src/data/nif.java
+++ src/data/nif.java
@@ -1,4 +1,56 @@
 package data;
 
-public class nif {
-}
+import exceptions.WrongNifFormatException;
+
+/**
+ * Essential data classes
+ */
+
+final public class Nif {
+    private final String nif;
+    public Nif (String code) throws WrongNifFormatException {
+        checkNif(code);
+        this.nif = code; }
+
+    private void checkNif(String code) throws WrongNifFormatException {
+        int correctNifLength = 9;
+
+        if (code == null) throw new NullPointerException("El nif es null");
+
+        if (code.length() != correctNifLength)
+            throw new WrongNifFormatException("La longitud del DNI no es la correcta");
+
+        if (wrongNifFormat(code))
+            throw new WrongNifFormatException("El format  no es el correcto. (8 números y una letra mayuscula)");
+    }
+
+    private boolean wrongNifFormat(String code) {
+        int correctNumbers = 8;
+        int correctCapitalLetters = 1;
+
+        int capitalLetters = 0;
+        int numbers = 0;
+
+        char[] nif = code.toCharArray();
+        for (char c : nif) {
+            if (Character.isDigit(c)) numbers++;
+            else if (Character.isUpperCase(c)) capitalLetters++;
+        }
+        return (numbers != correctNumbers || capitalLetters != correctCapitalLetters);
+    }
+
+    public String getNif () {
+        return nif; }
+
+    @Override
+    public boolean equals (Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false; Nif niff = (Nif) o;
+        return nif.equals(niff.nif);
+    }
+    @Override
+    public int hashCode () { return nif.hashCode(); }
+    @Override
+    public String toString () {
+        return "Nif{" + "nif ciudadano='" + nif + '\'' + '}';
+    } }
