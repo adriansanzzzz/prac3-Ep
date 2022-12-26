eb707e4674356bc4f89a4c14b2415fbc91135649
adriansanzzzz <74414073+adriansanzzzz@users.noreply.github.com>
2022-12-26 12:52:29 +0100

    -clase niftest y su interfaz

--- src/data/DocPath.java
+++ src/data/DocPath.java
@@ -1,4 +1,41 @@
 package data;
 
-public class DocPath {
+/**
+ * Essential data classes
+ */
+final public class DocPath {
+    private final String path;
+
+    public DocPath(String docPath) {
+        checkDocPath(docPath);
+
+        this.path = docPath;
+    }
+
+    private void checkDocPath(String docPath) {
+        if (docPath == null) throw new NullPointerException("El path del document és null");
+    }
+
+    public String getDocPath() {
+        return path;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DocPath doc_path = (DocPath) o;
+        return path.equals(doc_path.path);
+    }
+
+    @Override
+    public int hashCode() {
+        return path.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "DocPath{" + "path documento='" + path + '\'' + '}';
+    }
 }
+
---
+++ test/interfaces/NifTestInterface.java
@@ -0,0 +1,8 @@
+package interfaces;
+
+import org.junit.jupiter.api.Test;
+
+public interface NifTestInterface {
+    @Test
+    void getNifTest();
+}
