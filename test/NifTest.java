7f4ae964265bf18d8c832201fbcbf1de8c80ab4d
adriansanzzzz <74414073+adriansanzzzz@users.noreply.github.com>
2022-12-26 14:38:36 +0100

    Nif class and interface

---
+++ test/NifTest.java
@@ -0,0 +1,23 @@
+import data.Nif;
+import exceptions.WrongNifFormatException;
+import interfaces.NifTestInterface;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NifTest implements NifTestInterface {
+    Nif nif;
+
+    @BeforeEach
+    public void setUp() throws WrongNifFormatException {
+        String correctNif = "12345678A";
+        nif = new Nif(correctNif);
+    }
+
+    @Test
+    @Override
+    public void getNifTest() {
+        String correctNif = "12345678A";
+        assertEquals(correctNif, nif.getNif());
+    }
+}
