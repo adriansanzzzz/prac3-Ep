4887ff0049b1343c67242a4125bded5b885e3261
adriansanzzzz <74414073+adriansanzzzz@users.noreply.github.com>
2022-12-26 13:02:50 +0100

    -clase nif,niftest y su interfaz

--- test/interfaces/NifTestInterface.java
+++ test/interfaces/NifTestInterface.java
@@ -1,8 +1,60 @@
 package interfaces;
 
+import data.Nif;
+import exceptions.WrongNifFormatException;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public interface NifTestInterface {
     @Test
     void getNifTest();
+    @Test
+    default void getNullPointerNifTest() {
+        assertThrows(NullPointerException.class,
+                () -> new Nif(null));
+    }
+    @Test
+    default void tooFewNumbersTest() {
+        assertThrows(WrongNifFormatException.class,
+                () -> {
+                    String fewNumbers = "123A";
+                    new Nif(fewNumbers);
+                });
+    }
+    @Test
+    default void tooManyNumbersTest() {
+        assertThrows(WrongNifFormatException.class,
+                () -> {
+                    String manyNumbers = "123456789B";
+                    new Nif(manyNumbers);
+                });
+    }
+    @Test
+    default void tooFewLettersTest() {
+        assertThrows(WrongNifFormatException.class,
+                () -> {
+                    String fewLetters = "12345678";
+                    new Nif(fewLetters);
+                });
+    }
+    @Test
+    default void tooManyLettersTest() {
+        assertThrows(WrongNifFormatException.class,
+                () -> {
+                    String manyLetters = "12345678ACB";
+                    new Nif(manyLetters);
+                });
+    }
+
+    @Test
+    default void lowerCaseLettersTest() {
+        assertThrows(WrongNifFormatException.class,
+                () -> {
+                    String lowerCaseLetters = "12345678a";
+                    new Nif(lowerCaseLetters);
+                });
+    }
+
+
 }
