d2143ca0c01783bda2061526141977ef1a0892ce
adriansanzzzz <74414073+adriansanzzzz@users.noreply.github.com>
2022-12-26 15:03:55 +0100

    docpath

--- test/DocPathTest.java
+++ test/DocPathTest.java
@@ -1,5 +1,25 @@
+import data.DocPath;
 import interfaces.DocPathInterface;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DocPathTest implements DocPathInterface{
+    DocPath docPath;
+
+    @BeforeEach
+    public void setUp() {
+        String correctDocPath = "/Users/adriansanz/Desktop/Universidad/EP/test.out";
+        docPath = new DocPath(correctDocPath);
+    }
+
+    @Test
+    @Override
+    public void getDocPathTest() {
+        String correctDocPath = "/Users/adriansanz/Desktop/Universidad/EP/test.out";
+        assertEquals(correctDocPath, docPath.getDocPath());
+    }
+
 
 }
